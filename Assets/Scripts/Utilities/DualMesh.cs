70590bed33015f72c6c5a45ae5d6473483146e0f
Vishnu Menon <54286371+vishnumnn@users.noreply.github.com>
2020-12-14 23:44:26 -0600

    red-black tree re-color added

--- Assets/Scripts/Utilities/DualMesh.cs
+++ Assets/Scripts/Utilities/DualMesh.cs
@@ -8,11 +8,38 @@ namespace Assets.Scripts.Utilities
 {
     public class DualMesh
     {
-        Center[] centers;
+        private Center[] centers;
+        private List<Vertex> vertices;
 
         public DualMesh(int pointCount)
         {
             centers = new Center[pointCount];
+            vertices = new List<Vertex>(pointCount);
+            this.CreateCenters(centers);
+        }
+
+        private void CreateCenters(Center[] centers)
+        {
+            for(int i = 0; i < centers.Length; i++)
+            {
+                Random x = new Random(20);
+                centers[i] = new Center((float) x.NextDouble(), (float) x.NextDouble(), (float) x.NextDouble(), i);
+            }
+        }
+
+        private void RelaxCenters(Center[] centers)
+        {
+
+        }
+
+        private void CreateVoronoiDiagram(Center[] centers)
+        {
+            
+        }
+
+        private void CreateTriangulation(Center[] centers)
+        {
+
         }
 
         public int Size()
---
+++ Assets/Scripts/Utilities/RedBlackTree.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Utilities
+{
+    internal class RedBlackTree<T> where T : IComparable
+    {
+        private class Node
+        {
+            public T val { get; set; }
+            public Node left { get; set; }
+            public Node right { get; set; }
+            public Node parent { get; set; }
+            public bool color { get; set; }
+            public Node(T val, bool color)
+            {
+                this.val = val;
+                this.color = color;
+                left = null;
+                right = null;
+                parent = null;
+            }
+        }
+
+        private Node head { get; set; }
+        RedBlackTree()
+        {
+            head = null;
+        }
+
+        void Insert(T val)
+        {
+           if(head == null)
+            {
+                head = new Node(val, false);
+                return;
+            }
+            Node curr = head;
+            while(curr != null)
+            {
+                if(curr.val.CompareTo(val) >= 0 && curr.left == null)
+                {
+                    curr.left = new Node(val, true);
+                    curr.left.parent = curr;
+                    curr = curr.left;
+                    break;
+                }
+                else if(curr.val.CompareTo(val) >= 0)
+                {
+                    curr = curr.left;
+                }
+                else if (curr.val.CompareTo(val) < 0 && curr.right == null)
+                {
+                    curr.right = new Node(val, true);
+                    curr.right.parent = curr;
+                    curr = curr.right;
+                    break;
+                }
+                else
+                {
+                    curr = curr.right;
+                }
+            }
+            // Rebalance Tree if necessary
+            Rebalance(curr);
+        }
+
+        void Remove(T val)
+        {
+
+        }
+
+        void Rebalance(Node node)
+        {
+            Node par = node.parent;
+            while(par != null && par.color)
+            {
+                Node uncle = par == par.parent.left ? par.parent.right : par.parent.left;
+                if (uncle.color) // re-color only
+                {
+                    par.color = false;
+                    uncle.color = false;
+                    par.parent.color = true;
+                }
+                else // rotate and re-color
+                {
+
+                }
+                node = par;
+                par = node.parent;
+            }
+            if (par == null && node.color)
+            {
+                node.color = false;
+            }
+        }
+    }
+}
